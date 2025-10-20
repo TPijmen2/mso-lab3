2e2e877b00c70ba4603ebe735d4bf6106d30f956
Tijmen de Graaf <graafdiep@gmail.com>
2025-10-20 16:20:00 +0200

    test: Add unit tests for PathfindingExercise and exceptions

---
+++ ProgrammingLearningApp.Tests/CustomExceptionTests.cs
@@ -0,0 +1,36 @@
+﻿using ProgrammingLearningApp.Exceptions;
+using Xunit;
+
+namespace ProgrammingLearningApp.Tests
+{
+	public class CustomExceptionTests
+	{
+		[Fact]
+		public void OutOfBoundsException_StoresPosition()
+		{
+			// Arrange
+			var position = new Position(10, 10);
+
+			// Act
+			var exception = new OutOfBoundsException(position);
+
+			// Assert
+			Assert.Equal(position, exception.AttemptedPosition);
+			Assert.Contains("(10,10)", exception.Message);
+		}
+
+		[Fact]
+		public void BlockedCellException_StoresPosition()
+		{
+			// Arrange
+			var position = new Position(5, 5);
+
+			// Act
+			var exception = new BlockedCellException(position);
+
+			// Assert
+			Assert.Equal(position, exception.BlockedPosition);
+			Assert.Contains("(5,5)", exception.Message);
+		}
+	}
+}
---
+++ ProgrammingLearningApp.Tests/PathfindingExerciseTests.cs
@@ -0,0 +1,90 @@
+﻿using ProgrammingLearningApp.Models;
+using Xunit;
+
+namespace ProgrammingLearningApp.Tests
+{
+	public class PathfindingExerciseTests
+	{
+		[Fact]
+		public void Exercise_Constructor_SetsProperties()
+		{
+			// Arrange
+			var grid = new Grid(5, 5);
+			grid.SetEndPosition(4, 4);
+			var startPos = new Position(0, 0);
+
+			// Act
+			var exercise = new PathfindingExercise("Test", grid, startPos);
+
+			// Assert
+			Assert.Equal("Test", exercise.Name);
+			Assert.Equal(grid, exercise.Grid);
+			Assert.Equal(startPos, exercise.StartPosition);
+		}
+
+		[Fact]
+		public void Exercise_Constructor_DefaultsToBottomLeft()
+		{
+			// Arrange
+			var grid = new Grid(5, 5);
+			grid.SetEndPosition(4, 4);
+
+			// Act
+			var exercise = new PathfindingExercise("Test", grid);
+
+			// Assert
+			// Default start position is bottom-left corner: (0, Grid.Height - 1)
+			Assert.Equal(0, exercise.StartPosition.X);
+			Assert.Equal(4, exercise.StartPosition.Y); // Height - 1 = 5 - 1 = 4
+		}
+
+		[Fact]
+		public void Exercise_Constructor_ThrowsOnNullGrid()
+		{
+			// Act & Assert
+			Assert.Throws<ArgumentNullException>(() =>
+				new PathfindingExercise("Test", null));
+		}
+
+		[Fact]
+		public void Exercise_Constructor_ThrowsOnEmptyName()
+		{
+			// Arrange
+			var grid = new Grid(5, 5);
+
+			// Act & Assert
+			Assert.Throws<ArgumentException>(() =>
+				new PathfindingExercise("", grid));
+		}
+
+		[Fact]
+		public void IsCompleted_AtEndPosition_ReturnsTrue()
+		{
+			// Arrange
+			var grid = new Grid(5, 5);
+			grid.SetEndPosition(4, 4);
+			var exercise = new PathfindingExercise("Test", grid);
+
+			// Act
+			bool completed = exercise.IsCompleted(new Position(4, 4));
+
+			// Assert
+			Assert.True(completed);
+		}
+
+		[Fact]
+		public void IsCompleted_NotAtEndPosition_ReturnsFalse()
+		{
+			// Arrange
+			var grid = new Grid(5, 5);
+			grid.SetEndPosition(4, 4);
+			var exercise = new PathfindingExercise("Test", grid);
+
+			// Act
+			bool completed = exercise.IsCompleted(new Position(2, 2));
+
+			// Assert
+			Assert.False(completed);
+		}
+	}
+}
