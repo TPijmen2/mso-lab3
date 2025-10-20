5e3d4523e7640dcd161a4815fc5e6b67bff15df9
Tijmen de Graaf <graafdiep@gmail.com>
2025-10-20 14:45:00 +0200

    feat: Add custom exceptions for grid-aware execution
    - OutOfBoundsException: when moving outside grid
    - BlockedCellException: when hitting blocked cell
    - Both include position information for debugging

---
+++ ProgrammingLearningApp/Exceptions/BlockedCellException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgrammingLearningApp.Exceptions
+{
+	public class BlockedCellException : Exception
+	{
+		public Position BlockedPosition { get; }
+
+		public BlockedCellException(Position position)
+			: base($"Attempted to move to blocked cell at position {position}")
+		{
+			BlockedPosition = position;
+		}
+
+		public BlockedCellException(Position position, string message)
+			: base(message)
+		{
+			BlockedPosition = position;
+		}
+	}
+}
---
+++ ProgrammingLearningApp/Exceptions/OutOfBoundsException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgrammingLearningApp.Exceptions
+{
+	public class OutOfBoundsException : Exception
+	{
+		public Position AttemptedPosition { get; }
+
+		public OutOfBoundsException(Position position)
+			: base($"Attempted to move to position {position} which is outside the grid bounds")
+		{
+			AttemptedPosition = position;
+		}
+
+		public OutOfBoundsException(Position position, string message)
+			: base(message)
+		{
+			AttemptedPosition = position;
+		}
+	}
+}
