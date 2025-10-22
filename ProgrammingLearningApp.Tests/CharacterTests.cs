2ee3e25baabee5840972d4c4177349140a175c8c
Tijmen de Graaf <graafdiep@gmail.com>
2025-10-22 16:45:00 +0200

    test: Update character tests for grid-awareness

--- ProgrammingLearningApp.Tests/CharacterTests.cs
+++ ProgrammingLearningApp.Tests/CharacterTests.cs
@@ -1,6 +1,4 @@
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-using ProgrammingLearningApp;
-using System.Windows.Input;
+using ProgrammingLearningApp.Models;
 using Xunit;
 
 namespace ProgrammingLearningApp.Tests
