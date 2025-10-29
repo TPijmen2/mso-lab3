4f6d62d20ac66701829f1b32e262a56e0b623f65
Tijmen de Graaf <graafdiep@gmail.com>
2025-10-29 12:45:00 +0100

    feat: Implement TextProgramExporter

---
+++ ProgrammingLearningApp/Services/Exporters/TextProgramExporter.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace ProgrammingLearningApp.Services
+{
+	public class TextProgramExporter : IProgramExporter
+	{
+		public void Export(Program program, string filePath)
+		{
+			if (program == null)
+				throw new ArgumentNullException(nameof(program));
+
+			if (string.IsNullOrWhiteSpace(filePath))
+				throw new ArgumentException("File path cannot be empty");
+
+			string content = program.GetTextRepresentation();
+			File.WriteAllText(filePath, content);
+		}
+
+		public string GetFileExtension()
+		{
+			return ".txt";
+		}
+
+		public string GetFormatName()
+		{
+			return "Text Format";
+		}
+	}
+}
