27810dfc5a9d46835b5590822912315276d70722
Ilyas <obviouslygreen.official@gmail.com>
2025-10-30 11:15:00 +0100

    feat: Add ILogger interface for Observer pattern

---
+++ ProgrammingLearningApp/Services/Logger/ILogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgrammingLearningApp.Services.Logger
+{
+	public interface ILogger
+	{
+		void Log(string message, LogLevel level = LogLevel.Info);
+		void LogInfo(string message);
+		void LogWarning(string message);
+		void LogError(string message);
+		void LogDebug(string message);
+	}
+
+	public enum LogLevel
+	{
+		Debug,
+		Info,
+		Warning,
+		Error
+	}
+}
